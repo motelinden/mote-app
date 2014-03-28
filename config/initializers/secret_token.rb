64f3319c0867ecbc08bbbaae8942672b60e3596b
mote <motelinden@gmail.com>
2014-03-28 15:42:26 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MoteApp::Application.config.secret_key_base = '9cc1fb7054d19427ae2a60fcdb8440f03931f816084512927514b7d71f504eb0c7b9f8a0fc98f8430801a3bbbc7ad1caa209c9b80b830b9c9cb0d41c159fd4a2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MoteApp::Application.config.session_store :cookie_store, key: '_mote_app_session'
