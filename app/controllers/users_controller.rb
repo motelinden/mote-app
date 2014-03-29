f4d6157dc4cae4e07569ae33f4faa41b9dcd360e
mote <motelinden@gmail.com>
2014-03-30 07:31:48 +0800

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,29 @@
 class UsersController < ApplicationController
+	def show
+		@user = User.find(params[:id])
+		@title = @user.name
+	end
+
   def new
 		@title = 'Sign Up'
+		@user = User.new
   end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			flash[:success] = "Welcome to the Sample App!"
+			redirect_to @user
+		else
+			@title = "Sign Up"
+			render 'new'
+		end
+	end
+
+	 private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 	def full_title(page_title)
 		base_title = 'Mote App'
-		if page_title.empty? 
+		if page_title.nil? || page_title.empty? 
 			base_title
 		else
 			"#{base_title} | #{page_title}"
